bbaf97e7cad89700b9514fc8e2c5fdf855dde1a0
chugjugloli <cadenallen97@gmail.com>
2022-06-10 13:13:12 -0400

    learning js

---
+++ WEEK 3/JAVACRASHCOURSE/index.js
@@ -0,0 +1,25 @@
+/* Data Types:
+undefined, null, boolean, string, symbol, number, and object
+*/
+
+var myName = "Caden";
+// Used anywhere
+
+myName = 8;
+
+let ourName = "Caden Allen";
+//Used within scope of local area
+
+const pi = 3.14;
+//Can not change const = constant
+
+var a;
+var b = 2;
+
+a = 7;
+
+b = a;
+
+console.log(b);
+
+var a = 9;
